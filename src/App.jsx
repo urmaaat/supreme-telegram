9ad410e7b285cb2b6332bce1d3cdc7925eff848b
urmaaat <urn.matyev@gmail.com>
2023-10-22 15:21:56 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Background } from './component/Bacground'
+
+export const App = () => {
+  return (
+    <div>
+      <span>Its App!!</span>
+      <Background />
+    </div>
+  )
+}
---
+++ src/component/Bacground.jsx
@@ -0,0 +1,6 @@
+import styles from './Background.module.css'
+export cost Background = () => {
+    return (
+        <div className=''></div>
+    )
+}
